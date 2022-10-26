404851bd804d886a2bf1c85b09387d817a1affc1
Hamjad <dajmahsirdi@gmail.com>
2022-10-27 00:18:03 +0100

    Update signInRoute.js

--- Routes/signInRoute.js
+++ Routes/signInRoute.js
@@ -15,7 +15,7 @@ router.post('/', (req,res) => {
            return db.select('*')
             .from('users')
             .where("email",'=',email)
-            .then((users) => {res.json(users[0])
+            .then((users) => {res.json(users[0].id)
             }).catch ((err) => res.status(400).json('Unable to get user info'))
          } else {
             res.status(400).json('Wrong user info')
