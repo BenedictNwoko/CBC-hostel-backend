e9036d423ad6fcdb244a838738872a1a5acc0bf5
Hamjad Idris <dajmahsirdi@gmail.com>
2022-10-29 21:30:15 +0100

    checking existing email

--- Routes/registerRoute.js
+++ Routes/registerRoute.js
@@ -24,51 +24,51 @@ router.post("/", (req, res) => {
     } = req.body;
 
     db.select("email")
-      .from("users")
-      .where("email", "=", email)
-      .then((data) => {
-        if (data && data.length) {
-            return res.status(400).json({msg: `The email ${email} is already in use... try with another`,});
-        } else {
-          console.log(data.length);
-          const hash = bcrypt.hashSync(password, 10);
-          db.transaction((trx) => {
-            trx.insert({
-                hash: hash,
-                email: email,
-              })
-              .into("signedIn")
-              .returning("email")
-              .then((loginEmail) => {
-                return trx("users")
-                  .returning("*")
-                  .insert({
-                    email: loginEmail[0].email,
-                    fullname: fullname,
-                    phoneNumber: phoneNumber,
-                    hostel: hostel,
-                    gender: gender,
-                    maritalStatus: maritalStatus,
-                    age: age,
-                    occupation: occupation,
-                    disabilities: disabilities,
-                    nextOfKinName: nextOfKinName,
-                    nextOfKinEmail: nextOfKinEmail,
-                    nextOfKinPhoneNumber: nextOfKinPhoneNumber,
-                    nextOfKinAddress: nextOfKinAddress,
-                  })
-                  .then((users) => {
-                    res.json(users[0]);
-                  });
-              })
-              .then(trx.commit)
-              .catch(trx.rollback);
-          }).catch((err) => res.status(400).json("Unable to register user"));
-        }
-      });
-  } catch (err) {
-    console.log(err.message);
-  }
+    .from("users")
+    .where("email", "=", email)
+    .then((data) => {
+      if (data && data.length) {
+          return res.status(400).json({msg: `The email ${email} is already in use... try with another`,});
+      } else {
+        console.log(data.length);
+        const hash = bcrypt.hashSync(password, 10);
+        db.transaction((trx) => {
+          trx.insert({
+              hash: hash,
+              email: email,
+            })
+            .into("signedIn")
+            .returning("email")
+            .then((loginEmail) => {
+              return trx("users")
+                .returning("*")
+                .insert({
+                  email: loginEmail[0].email,
+                  fullname: fullname,
+                  phoneNumber: phoneNumber,
+                  hostel: hostel,
+                  gender: gender,
+                  maritalStatus: maritalStatus,
+                  age: age,
+                  occupation: occupation,
+                  disabilities: disabilities,
+                  nextOfKinName: nextOfKinName,
+                  nextOfKinEmail: nextOfKinEmail,
+                  nextOfKinPhoneNumber: nextOfKinPhoneNumber,
+                  nextOfKinAddress: nextOfKinAddress,
+                })
+                .then((users) => {
+                  res.json(users[0]);
+                });
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
+        }).catch((err) => res.status(400).json("Unable to register user"));
+      }
+    });
+} catch (err) {
+  console.log(err.message);
+}
 });
 
 module.exports = router;
