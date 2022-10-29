580e24e250db1ea360c7d67587d56f67a99e43bf
Hamjad Idris <dajmahsirdi@gmail.com>
2022-10-29 20:22:58 +0100

    checking for existing email

--- Routes/profileRoute.js
+++ Routes/profileRoute.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const { db } = require('../database')
 
 
 router.get('/', (req,res) => {
-    res.send('profile')
+    //console.log(req.params.id)
+        // const { id }  = req.params
+        // let found = false
+    
+        // db.select('*').from('users').where({
+        //     id:id
+        // }).then(users=>{
+        //     if (users.length){
+        //         res.json(users[0])
+        //     }else{
+        //         res.status(400).json('not found')
+        //     }
+        // })
+        // .catch(err => res.status('400').json('not found'))
+
 })
 
 
--- Routes/registerRoute.js
+++ Routes/registerRoute.js
@@ -1,35 +1,48 @@
-const express = require('express')
-const bcrypt = require('bcrypt')
-const router = express.Router()
-const knex = require('knex')
-const { db } = require('../database')
+const express = require("express");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+const knex = require("knex");
+const { db } = require("../database");
 
+router.post("/", (req, res) => {
+  try {
+    const {
+      fullname,
+      email,
+      password,
+      phoneNumber,
+      hostel,
+      gender,
+      maritalStatus,
+      age,
+      occupation,
+      disabilities,
+      nextOfKinName,
+      nextOfKinEmail,
+      nextOfKinPhoneNumber,
+      nextOfKinAddress,
+    } = req.body;
 
-router.post('/', (req,res) => {
-    try{
-        const { fullname, 
-                email, 
-                password,
-                phoneNumber,
-                hostel,
-                gender,
-                maritalStatus,
-                age,
-                occupation,
-                disabilities,
-                nextOfKinName,
-                nextOfKinEmail,
-                nextOfKinPhoneNumber,
-                nextOfKinAddress } = req.body
-        const hash = bcrypt.hashSync(password,10)
-        db.transaction(trx => {
+    db.select("email")
+      .from("users")
+      .where("email", "=", email)
+      .then((data) => {
+        if (data && data.length) {
+            return res.status(400).json({msg: `The email ${email} is already in use... try with another`,});
+        } else {
+          console.log(data.length);
+          const hash = bcrypt.hashSync(password, 10);
+          db.transaction((trx) => {
             trx.insert({
-                hash:hash,
-                email:email
-            }).into('signedIn').returning('email').then(loginEmail => {
-                return trx('users')
-                .returning('*')
-                .insert({
+                hash: hash,
+                email: email,
+              })
+              .into("signedIn")
+              .returning("email")
+              .then((loginEmail) => {
+                return trx("users")
+                  .returning("*")
+                  .insert({
                     email: loginEmail[0].email,
                     fullname: fullname,
                     phoneNumber: phoneNumber,
@@ -42,17 +55,20 @@ router.post('/', (req,res) => {
                     nextOfKinName: nextOfKinName,
                     nextOfKinEmail: nextOfKinEmail,
                     nextOfKinPhoneNumber: nextOfKinPhoneNumber,
-                    nextOfKinAddress: nextOfKinAddress
-                    
-                }).then(
-                    users => {res.json(users[0])}
-                )
-            }).then(trx.commit)
-            .catch(trx.rollback)
-        }).catch(err => res.status(400).json('Unable to register user'))
-    } catch(err){
-        console.log(err.message)
-    }
-        })
-   
-module.exports = router
+                    nextOfKinAddress: nextOfKinAddress,
+                  })
+                  .then((users) => {
+                    res.json(users[0]);
+                  });
+              })
+              .then(trx.commit)
+              .catch(trx.rollback);
+          }).catch((err) => res.status(400).json("Unable to register user"));
+        }
+      });
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+module.exports = router;
